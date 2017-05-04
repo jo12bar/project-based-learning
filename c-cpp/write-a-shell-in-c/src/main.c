2fbf081c42dd498a69c1e575a3d3590b35092409
jo12bar <johann.b@telus.net>
2017-05-04 16:47:04 -0700

    Implement lsh_read_line

--- c-cpp/write-a-shell-in-c/src/main.c
+++ c-cpp/write-a-shell-in-c/src/main.c
@@ -16,13 +16,48 @@
 #include <stdio.h>
 #include <string.h>
 
+#define LSH_RL_BUFSIZE 1024
 /**
  * @brief Read a line of input from stdin.
  * @return The line from stdin.
  */
 char *lsh_read_line(void) {
-  // TODO
-  return "foo";
+  int bufsize = LSH_RL_BUFSIZE;
+  int position = 0;
+  char *buffer = malloc(sizeof(char) * bufsize);
+  int c;
+
+  if (!buffer) {
+    fprintf(stderr, "lsh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  while (1) {
+    // Read a character
+    c = getchar();
+
+    // If we hit EOF or '\n', replace it with a null character and return.
+    if (c == EOF || c == '\n') {
+      buffer[position] = '\0';
+      return buffer;
+    } else {
+      buffer[position] = c;
+    }
+
+    position++;
+
+    // If we have exceeded the buffer, reallocate.
+    if (position >= bufsize) {
+      bufsize += LSH_RL_BUFSIZE;
+      
+      buffer = realloc(buffer, bufsize);
+
+      if (!buffer) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
 }
 
 /**
@@ -54,11 +89,11 @@ void lsh_loop(void) {
 
   do {
     printf("> ");
-    //line = lsh_read_line();
+    line = lsh_read_line();
     //args = lsh_split_line(line);
     //status = lsh_execute(args);
     status = 0; // TODO: Remove this!
-    //free(line);
+    free(line);
     //free(args);
   } while(status);
 }
