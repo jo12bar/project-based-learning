727b131285be0a209c57584427ffee67fe9d46e0
jo12bar <johann.b@telus.net>
2017-05-04 19:02:16 -0700

    Implement lsh_launch

--- c-cpp/write-a-shell-in-c/src/main.c
+++ c-cpp/write-a-shell-in-c/src/main.c
@@ -17,6 +17,39 @@
 #include <string.h>
 
 /**
+ * @brief Launch a program and wait for it to terminate.
+ * @param args NULL-terminated list of arguments (including program)
+ * @return Always returns 1, to continue execution.
+ */
+int lsh_launch(char **args) {
+  pid_t pid;
+  pid_t wpid;
+
+  int status;
+  
+  // Duplicate this process.
+  pid = fork();
+
+  if (pid == 0) {
+    // This is the child process! Replace it with args[0].
+    if (execvp(args[0], args) == -1) {
+      perror("lsh");
+    }
+    exit(EXIT_FAILURE);
+  } else if (pid < 0) {
+    // Error forking.
+    perror("lsh");
+  } else {
+    // This is the parent process!
+    do {
+      wpid = waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+
+  return 1;
+}
+
+/**
  * @brief Execute shell built-in or launch program.
  * @param args NULL-terminated list of arguments.
  * @return 1 if the shell should continue running, or 0 if it should terminate.
