a80b94b17f0dc20994f444565f5d990e1699a921
jo12bar <johann.b@telus.net>
2017-05-07 17:53:12 -0700

    Reset the terminal's attributes on exit
    Apparantly, people don't like it when their shell is broken after running a
    program.

--- c-cpp/build-a-text-editor/kilo.c
+++ c-cpp/build-a-text-editor/kilo.c
@@ -1,16 +1,41 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+/**
+ * @brief The original attributes for termios
+ */
+struct termios orig_termios;
+
+/**
+ * @brief Reset the terminal's attributes so that the user's shell isn't broken.
+ */
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+/**
+ * @brief Enable the terminal's raw mode.
+ */
 void enableRawMode() {
-  struct termios raw;
+  // Save the terminal's original attributes, and reset them on exit (so that
+  // the user's shell doesn't break).
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
 
-  tcgetattr(STDIN_FILENO, &raw);
+  // Copy of the terminal's attributes to be modified.
+  struct termios raw = orig_termios;
 
+  // Turn off echoing.
   raw.c_lflag &= ~(ECHO);
 
+  // Tell the terminal to use our modified attributes.
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
+/**
+ * @brief Main entry point.
+ */
 int main() {
   enableRawMode();
 
